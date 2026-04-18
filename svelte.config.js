a28162186f7c3e3b113749d876b36f4d6aef117d
Sebastian Nepote <snepote@gmail.com>
2026-04-19 01:53:35 +0200

    fix(warnings) svelte code

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
+	onwarn(warning, handler) {
+		if (warning.code === 'state_referenced_locally' && warning.filename?.includes('generated/root.svelte')) return;
+		handler(warning);
+	},
 	kit: {
 		adapter: adapter({
 			routes: {
